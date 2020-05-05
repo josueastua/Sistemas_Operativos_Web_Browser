fd58f0f8b3531b5992c25aa43c26c941c7396d67
josueastua <53876880+josueastua@users.noreply.github.com>
2020-05-05 00:40:48 -0600

    Adiciones.

--- Tarea1/Tarea1/Proceso.cs
+++ Tarea1/Tarea1/Proceso.cs
@@ -7,21 +7,27 @@ namespace Tarea1
     class Proceso
     {
         public int id;
+        public int[] libres;
         public int[] recursos;
         public int[] solicitud;//Cantidad de recursos que va a solicitar
         public int vida;//Tiempo total que requiere para ser ejecutado
         public int inanicion;//Tiempo maximo que esperara entes de ser elimincado de la cola de procesoso
         public int espera;//Tiempo que aguardara antes de solicitar nuevamente recursoso
+        public int tte;//Tiempo toltal esperado
         public Boolean estado;
 
         public Proceso(){
             estado = false;
+            inanicion = 0;
+            vida = 0;
+            espera = 0;
         }
 
-        public Proceso(int id, int[] recursos)
+        public Proceso(int id, int[] recursos, int[] libres)
         {
             this.id = id;
             this.recursos = recursos;
+            this.libres = libres;
             solicitud = new int[recursos.Length];
             iniciarVariables();
         }
@@ -32,17 +38,42 @@ namespace Tarea1
         public int getInanicion() => inanicion;
         public int getEspera() => espera;
         public Boolean getEstado() => estado;
+        public int getTte() => tte;
         public void iniciarVariables()
         {
             var random = new Random();
             vida = random.Next(1, 100);
-            inanicion = random.Next(0, 100);
-            espera = random.Next(1, 100);
-            for(int i = 0; i < recursos.Length; i++)
+            espera = random.Next(1, 60);
+            while (inanicion < espera)
+            {
+                inanicion = random.Next(0, 100);
+            }
+            for (int i = 0; i < recursos.Length; i++)
             {
                 solicitud[i] = random.Next(0, recursos[i] + 1);
             }
         }
         public void cambioEstado() => estado = !estado;
+
+        public Boolean puedeEjecutarse()
+        {
+            for(int i = 0; i < libres.Length; i++)
+            {
+                if(libres[i] < solicitud[i])
+                {
+                    return false;
+                }
+            }
+            restarRecursos();
+            return true; 
+        }
+
+        public void restarRecursos()
+        {
+            for (int i = 0; i < libres.Length; i++)
+            {
+                libres[i] -= solicitud[i];
+            }
+        }
     }
 }
